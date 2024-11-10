e6ef90861cce0d8f758579d0579a9bffa45fa07b
arungeorgesaji <arungeorgesaji@gmail.com>
2024-11-10 11:28:31 +0530

    Modified Arch's build.sh as in changed image

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_PATH="$(readlink -f "$0")"
+SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+XML_FILE="/tmp/debian-vm-base.xml"
+
+LATEST_IMAGE=$(lynx -dump -listonly -nonumbers https://releases.ubuntu.com | grep [0-9] | tail -n 1 | xargs lynx -dump -listonly -nonumbers | grep iso | grep live | grep -v zsync | grep -v torrent | head -n 1)
+
+echo y | ship --vm delete debian-vm-base 
+
+echo n | ship --vm create debian-vm-base --source "$LATEST_IMAGE"
+
+sed -i '/<\/devices>/i \
+  <console type="pty">\
+    <target type="virtio"/>\
+  </console>' "$XML_FILE"
+
+virsh -c qemu:///system undefine debian-vm-base
+virsh -c qemu:///system define "$XML_FILE"
+
+echo "Building of VM Complete.Starting might take a while as it might take a bit of type for the vm to boot up and be ready for usage."
+ship --vm start debian-vm-base 
